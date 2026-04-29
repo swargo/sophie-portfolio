d74ca28de0922970692490f0dbd2db3656f108b0
swargo <sophie.wargo@gmail.com>
2026-04-28 20:20:02 -0600

    remove bg decorations

--- app/_components/BackgroundDecorations/index.tsx
+++ app/_components/BackgroundDecorations/index.tsx
@@ -11,7 +11,7 @@ const Layer = styled.div`
 
 export const BackgroundDecorations = () => (
   <Layer aria-hidden="true">
-    {/* top-right: three concentric rings */}
+    {/* top-right: three rings */}
     <svg
       style={{ position: "absolute", top: -160, right: -160 }}
       width="560"
@@ -46,7 +46,7 @@ export const BackgroundDecorations = () => (
       />
     </svg>
 
-    {/* bottom-left: two peach rings */}
+    {/* bottom-left: two rings */}
     <svg
       style={{ position: "absolute", bottom: -130, left: -110 }}
       width="460"
@@ -67,106 +67,6 @@ export const BackgroundDecorations = () => (
         cx="230"
         cy="230"
         r="150"
-        stroke="#4B6B94"
-        strokeWidth="1.5"
-        opacity="0.05"
-      />
-    </svg>
-
-    {/* mid-left: dot cluster */}
-    <svg
-      style={{ position: "absolute", left: "7%", top: "42%" }}
-      width="88"
-      height="132"
-      viewBox="0 0 88 132"
-      fill="#4B6B94"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle cx="12" cy="16" r="5" opacity="0.05" />
-      <circle cx="36" cy="8" r="4" opacity="0.05" />
-      <circle cx="64" cy="20" r="6" opacity="0.05" />
-      <circle cx="20" cy="50" r="4" opacity="0.05" />
-      <circle cx="52" cy="44" r="7" opacity="0.05" />
-      <circle cx="10" cy="80" r="5" opacity="0.05" />
-      <circle cx="40" cy="88" r="4" opacity="0.05" />
-      <circle cx="70" cy="74" r="5" opacity="0.05" />
-      <circle cx="26" cy="116" r="4" opacity="0.05" />
-      <circle cx="60" cy="112" r="6" opacity="0.05" />
-    </svg>
-
-    {/* mid-right: diamond outline */}
-    <svg
-      style={{ position: "absolute", right: "6%", top: "52%" }}
-      width="72"
-      height="72"
-      viewBox="0 0 72 72"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect
-        x="6"
-        y="6"
-        width="60"
-        height="60"
-        stroke="##4B6B94"
-        strokeWidth="2"
-        opacity="0.05"
-        transform="rotate(45 36 36)"
-      />
-    </svg>
-
-    {/* upper-left: small cross */}
-    <svg
-      style={{ position: "absolute", left: "18%", top: "14%" }}
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <line
-        x1="12"
-        y1="0"
-        x2="12"
-        y2="24"
-        stroke="#4B6B94"
-        strokeWidth="1.5"
-        opacity="0.05"
-      />
-      <line
-        x1="0"
-        y1="12"
-        x2="24"
-        y2="12"
-        stroke="#4B6B94"
-        strokeWidth="1.5"
-        opacity="0.05"
-      />
-    </svg>
-
-    {/* lower-right: small cross */}
-    <svg
-      style={{ position: "absolute", right: "20%", bottom: "22%" }}
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <line
-        x1="12"
-        y1="0"
-        x2="12"
-        y2="24"
-        stroke="#4B6B94"
-        strokeWidth="1.5"
-        opacity="0.05"
-      />
-      <line
-        x1="0"
-        y1="12"
-        x2="24"
-        y2="12"
         stroke="#4B6B94"
         strokeWidth="1.5"
         opacity="0.05"
